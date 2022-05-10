6df22b18f301dfad923448e87872e9270b7ce75b
sylvain campos <campos.sylvain@gmail.com>
2022-05-10 10:11:35 +0200

    first exo ok

--- weatherAwait.js
+++ weatherAwait.js
@@ -1,5 +1,5 @@
 // recupération de la latitude et de la longitude depuis l'API opencagedata
-async function getGeometry(){
+async function getGeometry() {
     let where = document.getElementById("ville").value;
     where = encodeURIComponent(where)
     console.log(where);
@@ -10,19 +10,19 @@ async function getGeometry(){
     const lat = reponse.results[0].geometry.lat;
     const lng = reponse.results[0].geometry.lng;
     console.log(lat + " " + lng);
-    getMeteo(lat,lng);
+    getMeteo(lat, lng);
 }
 
 
 // recupération de la méteo du lieu recherché
-async function getMeteo(lat,lng){
+async function getMeteo(lat, lng) {
     const API_meteo_KEY = "9612067697a364bf80ed7e252bbc95e2";
     let urlMeteo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${API_meteo_KEY}`
     let rep = await fetch(urlMeteo);
     let reponse = await rep.json();
     temps = reponse.current.weather[0].id;
     cloudsPercent = reponse.current.clouds;
-    console.log(temps + " " + cloudsPercent);
+    console.log("id = " + temps + " " + "nuages " + cloudsPercent + "%");
     writeHTML(temps, cloudsPercent)
 }
 
@@ -31,12 +31,30 @@ async function getMeteo(lat,lng){
 function writeHTML(temps, cloudsPercent) {
     let toDay = day();
     let jour = document.createElement("h2");
-        jour.innerHTML = toDay;
-        resultat.appendChild(jour);
-    if (temps=== 800) {
-        let resultat = document.querySelector("#resultat");
-        let imgMeteo = document.createElement("img");
+    jour.innerHTML = toDay;
+    let resultat = document.querySelector("#resultat");
+    resultat.appendChild(jour);
+    let imgMeteo = document.createElement("img");
+    if (temps === 800) {
         imgMeteo.src = `./img/sun.svg`;
+        imgMeteo.style.width = "150px";
+        resultat.appendChild(imgMeteo);
+    } else if (temps > 600 && temps < 622) {
+        imgMeteo.src = `./img/snow.svg`;
+        imgMeteo.style.width = "150px";
+        resultat.appendChild(imgMeteo);
+    } else if (temps >= 801 && temps <= 804) {
+        if (cloudsPercent <= 50) {
+            imgMeteo.src = `./img/cloudy.svg`;
+            imgMeteo.style.width = "150px";
+            resultat.appendChild(imgMeteo);
+        } else {
+            imgMeteo.src = `./img/clouds.svg`;
+            imgMeteo.style.width = "150px";
+            resultat.appendChild(imgMeteo);
+        }
+    } else {
+        imgMeteo.src = `./img/rain.svg`;
         imgMeteo.style.width = "150px";
         resultat.appendChild(imgMeteo);
     }
@@ -50,7 +68,7 @@ function day() {
     return days[d.getDay()];
 }
 
-document.querySelector("#recherche").addEventListener("click", function(e){
+document.querySelector("#recherche").addEventListener("click", function (e) {
     e.preventDefault();
     document.querySelector("#resultat").innerHTML = "";
     getGeometry();
